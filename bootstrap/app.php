ca4a3059ef858875e76a8515eba091c14dc5a961
sarah <sarah.teoh752@gmail.com>
2024-06-26 06:31:32 +0800

    feat: return not found msg json in case of 404

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\AlwaysAcceptJson;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,5 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->prependToGroup('api', AlwaysAcceptJson::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->renderable(function (NotFoundHttpException $e) {
+            return response()->json(['message' => env('APP_DEBUG') ? $e->getMessage() : 'Object not found'], 404);
+        });
     })->create();

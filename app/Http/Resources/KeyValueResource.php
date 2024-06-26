e424ab059fa2c9b15301e521353849180bf6363a
sarah <sarah.teoh752@gmail.com>
2024-06-26 08:29:17 +0800

    feat: define key value resource

---
+++ app/Http/Resources/KeyValueResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property string $key
+ * @property int $timestamp
+ * @property string $value
+ */
+class KeyValueResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'key' => $this->key,
+            'value' => $this->value,
+            'timestamp' => $this->timestamp,
+        ];
+    }
+}
